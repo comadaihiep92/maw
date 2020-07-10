f19a00a43e2eab6f47ce67fa1fa06195897e2f63
comadaihiep92 <motden100@gmail.com>
2020-07-10 17:37:18 +0700

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,43 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: "splash",
+    loadChildren: () =>
+      import("./splash/splash.module").then((m) => m.SplashPageModule),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "home",
+    loadChildren: () =>
+      import("./home/home.module").then((m) => m.HomePageModule),
+  },
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
+  },
+  // {
+  //   path: "",
+  //   redirectTo: "home",
+  //   pathMatch: "full",
+  // },
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./login/login.module").then((m) => m.LoginPageModule),
+  },
+
+  {
+    path: "reg",
+    loadChildren: () => import("./reg/reg.module").then((m) => m.RegPageModule),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
