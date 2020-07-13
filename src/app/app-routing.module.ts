c0ea919e72799dab5d146179a8f56018738dfdfa
comadaihiep92 <motden100@gmail.com>
2020-07-13 11:57:19 +0700

    tab 1 home done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,32 @@ const routes: Routes = [
     path: "reg",
     loadChildren: () => import("./reg/reg.module").then((m) => m.RegPageModule),
   },
+  // {
+  //   path: "tabs",
+  //   loadChildren: () =>
+  //     import("./tabs/tabs.module").then((m) => m.TabsPageModule),
+  // },
+  { path: "", loadChildren: "./tabs/tabs.module#TabsPageModule" },
+  // {
+  //   path: "tab1",
+  //   loadChildren: () =>
+  //     import("./tab1/tab1.module").then((m) => m.Tab1PageModule),
+  // },
+  // {
+  //   path: "tab2",
+  //   loadChildren: () =>
+  //     import("./tab2/tab2.module").then((m) => m.Tab2PageModule),
+  // },
+  // {
+  //   path: "tab3",
+  //   loadChildren: () =>
+  //     import("./tab3/tab3.module").then((m) => m.Tab3PageModule),
+  // },
+  // {
+  //   path: "tab4",
+  //   loadChildren: () =>
+  //     import("./tab4/tab4.module").then((m) => m.Tab4PageModule),
+  // },
 ];
 
 @NgModule({
