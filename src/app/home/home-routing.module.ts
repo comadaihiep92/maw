534906474d3f09e4250fc1191044e0db5634f152
comadaihiep92 <motden100@gmail.com>
2020-07-17 14:08:03 +0700

    final commit

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -17,6 +17,21 @@ const routes: Routes = [
           },
         ],
       },
+      // {
+      //   path: "waiting",
+      //   loadChildren: "../waiting/waiting.module#WaitingPageModule",
+      //   redirectTo: "/home/tab2/waiting",
+      // },
+      // {
+      //   path: "sure",
+      //   loadChildren: "../waiting/waiting.module#WaitingPageModule",
+      //   redirectTo: "/home/tab2/sure",
+      // },
+      // {
+      //   path: "cancel",
+      //   loadChildren: "../waiting/waiting.module#WaitingPageModule",
+      //   redirectTo: "/home/tab2/cancel",
+      // },
       {
         path: "tab2",
         children: [
@@ -38,6 +53,7 @@ const routes: Routes = [
           // },
         ],
       },
+
       {
         path: "tab3",
         children: [
@@ -46,18 +62,18 @@ const routes: Routes = [
             loadChildren: "../tab3/tab3.module#Tab3PageModule",
           },
 
-          {
-            path: "waiting",
-            loadChildren: "../waiting/waiting.module#WaitingPageModule",
-          },
-          {
-            path: "sure",
-            loadChildren: "../sure/sure.module#SurePageModule",
-          },
-          {
-            path: "cancel",
-            loadChildren: "../cancel/cancel.module#CancelPageModule",
-          },
+          // {
+          //   path: "waiting",
+          //   loadChildren: "../waiting/waiting.module#WaitingPageModule",
+          // },
+          // {
+          //   path: "sure",
+          //   loadChildren: "../sure/sure.module#SurePageModule",
+          // },
+          // {
+          //   path: "cancel",
+          //   loadChildren: "../cancel/cancel.module#CancelPageModule",
+          // },
         ],
       },
       {
